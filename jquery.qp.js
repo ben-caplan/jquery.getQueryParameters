4e4f4b65abaef94b0c5f477ef747b3f080a6ac9d
Benjamin Caplan <ben@iamcaplan.com>
2014-02-23 11:31:15 -0500

    Extend Functionality
    Add ability for multiple query parameters to have the same name, by building an array of their values. Also add a version that extends the jQuery object for use as $.getQueryParameters().

---
+++ jquery.qp.js
@@ -0,0 +1,9 @@
+jQuery.extend({
+  getQueryParameters : function(str) {
+    return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){
+      return n = n.split("="),
+      ( !this[n[0]] ? this[n[0]] = n[1] : ((typeof this[n[0]] === 'object') ? this[n[0]].push(n[1]) : this[n[0]]=[this[n[0]], n[1]]) ),
+      this
+    }.bind({}))[0];
+  }
+});
--- qp.js
+++ qp.js
@@ -6,5 +6,11 @@
  */
 
 window.getQueryParameters = function(str) {
-  return (str || document.location.search).replace(/(^\?)/,'').split("&").map(function(n){return n = n.split("="),this[n[0]] = n[1],this}.bind({}))[0];
+  return (str || document.location.search).replace(/(^\?)/,'').split("&").map(
+  	function(n){
+  		//return n = n.split("="),this[n[0]] = n[1],this
+  		return n = n.split("="),
+		( !this[n[0]] ? this[n[0]] = n[1] : ((typeof this[n[0]] === 'object') ? this[n[0]].push(n[1]) : this[n[0]]=[this[n[0]], n[1]]) ),
+		this
+  }.bind({}))[0];
 }
